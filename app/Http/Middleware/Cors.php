9ba82162c1f978258b4665b2adf82417138bde03
Victor Tran Anh Vu <vu.trananh@gforces.co.uk>
2020-07-12 13:58:43 +0700

    - Fix cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,11 +15,10 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE,OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type');
-
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
     }
+
 }
