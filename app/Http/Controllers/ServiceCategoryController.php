505590a17ae4578a55619910b45f0083ffb27210
victortrananhvu <vuanhtran95@gmail.com>
2020-07-14 20:20:01 +0700

    - Service Categories

---
+++ app/Http/Controllers/ServiceCategoryController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller as Controller;
+use App\Http\HttpResponse;
+use App\Repositories\ServiceCategoryRepositoryInterface;
+use App\Repositories\ServiceRepositoryInterface;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response as Response;
+use Exception;
+use App\Helper\Translation;
+
+class ServiceCategoryController extends Controller
+{
+    private $serviceCategoryRepository;
+
+    public function __construct(ServiceCategoryRepositoryInterface $serviceCategoryRepository)
+    {
+        $this->serviceCategoryRepository = $serviceCategoryRepository;
+    }
+
+    public function create(Request $request)
+    {
+        $params = $request->all();
+        die(var_dump($params));
+        try {
+            $service = $this->serviceCategoryRepository->create($params);
+            if ($service) {
+                return HttpResponse::toJson(true,
+                    Response::HTTP_CREATED,
+                    Translation::$SERVICE_CATEGORY_CREATED,
+                    $service);
+            } else {
+                //TODO: Need to improve
+                return HttpResponse::toJson(false, Response::HTTP_BAD_REQUEST, Translation::$SYSTEM_ERROR);
+            }
+        } catch (Exception $e) {
+            return HttpResponse::toJson(false, Response::HTTP_CONFLICT, Translation::$SERVICE_CATEGORY_CREATE_ERROR);
+        }
+    }
+
+    public function get()
+    {
+        $service_categories = $this->serviceCategoryRepository->get();
+        if (!empty($service_categories)) {
+            return HttpResponse::toJson(true, Response::HTTP_OK, Translation::$GET_ALL_SERVICE_CATEGORY_SUCCESS, $services);
+        } else {
+            return HttpResponse::toJson(false, Response::HTTP_NOT_FOUND, Translation::$NO_SERVICE_CATEGORY_FOUND);
+        }
+    }
+
+    // TODO:
+    public function update(Request $request, $id)
+    {
+        $params = $request->all();
+        try {
+            $user = $this->serviceCategoryRepository->update($id, $params);
+            if ($user) {
+                return HttpResponse::toJson(true,
+                    Response::HTTP_OK,
+                    Translation::$SERVICE_CATEGORY_UPDATED, $user);
+            } else {
+                //TODO: Need to improve
+                return HttpResponse::toJson(false,
+                    Response::HTTP_BAD_REQUEST,
+                    Translation::$SYSTEM_ERROR);
+            }
+        } catch (Exception $e) {
+            return HttpResponse::toJson(false,
+                Response::HTTP_CONFLICT,
+                Translation::$SERVICE_CATEGORY_UPDATED_FAILURE);
+        }
+    }
+
+    // TODO:
+    public function delete($id)
+    {
+        try {
+            $delete = $this->serviceCategoryRepository->delete($id);
+            if ($delete) {
+                return HttpResponse::toJson(true,
+                    Response::HTTP_OK,
+                    Translation::$DELETE_SERVICE_CATEGORY_SUCCESS);
+            } else {
+                return HttpResponse::toJson(false,
+                    Response::HTTP_BAD_REQUEST,
+                    Translation::$DELETE_SERVICE_CATEGORY_FAILURE);
+            }
+        } catch (Exception $e) {
+            return HttpResponse::toJson(false,
+                Response::HTTP_CONFLICT,
+                Translation::$DELETE_SERVICE_CATEGORY_FAILURE);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -25,24 +25,22 @@ Route::get('/private', function (Request $request) {
 // Get All Roles
 Route::get('/v1/roles', 'RoleController@get')->middleware('auth:api');
 
-// Get User Info
+// User
 Route::get('/v1/userinfo', 'UserController@getUserInfo')->middleware('auth:api');
-// Create User
 Route::post('/v1/users', 'UserController@create')->middleware('auth:api');
-// Get All User
 Route::get('/v1/users', 'UserController@get')->middleware('auth:api');
-// Get One User By Id
 Route::get('/v1/users/{id}', 'UserController@getOneById')->middleware('auth:api');
-// Update One User By Id
 Route::put('/v1/users/{id}', 'UserController@update')->middleware('auth:api');
-// Delete One User By Id
 Route::delete('/v1/users/{id}', 'UserController@delete')->middleware('auth:api');
 
-// Get all services
+// Service
 Route::get('/v1/services', 'ServiceController@get')->middleware('auth:api');
-
-// Create service
 Route::post('/v1/services', 'ServiceController@create')->middleware('auth:api');
+
+// Service Category
+Route::get('/v1/service-categories', 'ServiceCategoryController@get')->middleware('auth:api');
+Route::post('/v1/service-categories', 'ServiceCategoryController@create')->middleware('auth:api');
+
 
 // Customer
 Route::post('/v1/customers', 'CustomerController@create')->middleware('auth:api');
