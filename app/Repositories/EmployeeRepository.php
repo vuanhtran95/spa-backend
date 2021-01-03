16dc781f6eea95f1b661a3d3dde164488e49f0ce
hmhuy <huy.hominh>
2021-01-03 16:08:54 +0700

    Fix employee commission

--- app/Repositories/EmployeeRepository.php
+++ app/Repositories/EmployeeRepository.php
@@ -76,37 +76,47 @@ class EmployeeRepository implements EmployeeRepositoryInterface
             $query = $query::where('role_id', $roleId);
         }
 
+        $date = Carbon::now()->setTimezone('Asia/Ho_Chi_Minh');
+
+        // Get this month commissions
+        $this_month_from =  $date->copy()->startOfMonth()->setTimezone('UTC')->toDateTimeString();
+        $this_month_to = $date->copy()->endOfMonth()->setTimezone('UTC')->toDateTimeString();
+
+        // Get last month commissions
+        $last_month_from =  $date->copy()->startOfMonth()->subMonths(1)->setTimezone('UTC')->toDateTimeString();
+        $last_month_to = $date->copy()->endOfMonth()->subMonths(1)->setTimezone('UTC')->toDateTimeString();
+
         // With commissions
         $query->withCount(['order AS working_commission' => function ($query) {
-            $query->whereYear('updated_at', Carbon::now()->year)->whereMonth('updated_at', Carbon::now()->month)
+            $query->whereBetween('updated_at', [$this_month_from, $this_month_to])
                 ->select(DB::raw("SUM(working_commission)"));
         }]);
         
         $query->withCount(['order AS working_commission_prev' => function ($query) {
-            $query->whereYear('updated_at', Carbon::now()->year)->whereMonth('updated_at', Carbon::now()->month-1)
+            $query->whereBetween('updated_at', [$last_month_from, $last_month_to])
                 ->select(DB::raw("SUM(working_commission)"));
         }]);
 
         $query->withCount(['package AS sale_commission' => function ($query) {
-            $query->whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)
+            $query->whereBetween('created_at', [$this_month_from, $this_month_to])
                 ->select(DB::raw("SUM(sale_commission)"));
         }]);
 
         $query->withCount(['package AS sale_commission_prev' => function ($query) {
-            $query->whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month-1)
+            $query->whereBetween('created_at', [$last_month_from, $last_month_to])
                 ->select(DB::raw("SUM(sale_commission)"));
         }]);
 
         // Count Orders
         $query->withCount(['order AS total_sales' => function ($query) {
-            $query->whereYear('updated_at', Carbon::now()->year)->whereMonth('updated_at', Carbon::now()->month)->whereHas('intake', function ($iQuery) {
+            $query->whereBetween('updated_at', [$this_month_from, $this_month_to])->whereHas('intake', function ($iQuery) {
                 $iQuery->where('is_valid', '=', 1);
             })->select(DB::raw("SUM(price)"));
             ;
         }]);
         
         $query->withCount(['order AS total_sales_prev' => function ($query) {
-            $query->whereYear('updated_at', Carbon::now()->year)->whereMonth('updated_at', Carbon::now()->month-1)->whereHas('intake', function ($iQuery) {
+            $query->whereBetween('updated_at', [$last_month_from, $last_month_to])->whereHas('intake', function ($iQuery) {
                 $iQuery->where('is_valid', '=', 1);
             })->select(DB::raw("SUM(price)"));
             ;
