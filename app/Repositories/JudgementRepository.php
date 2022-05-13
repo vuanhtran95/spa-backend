e90261634d6cdd5ff952ac21ad453c585d3a6af0
ShoHo <shoho@Hos-MacBook-Pro.local>
2022-05-13 13:33:41 +0700

    update judgement

--- app/Repositories/JudgementRepository.php
+++ app/Repositories/JudgementRepository.php
@@ -45,6 +45,7 @@ class JudgementRepository implements JudgementRepositoryInterface
         }
 
         $judgements = $query->orderBy('id', 'desc')
+                            ->with(['employee'])
                             ->get()
                             ->toArray();
         return [
