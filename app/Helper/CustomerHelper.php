04bb8e16b10ee83f3c61d4c5b329ce07e240e834
hmhuyit96 <hmhuy.it.work@gmail.com>
2023-04-04 01:04:58 +0700

    update logics

--- app/Helper/CustomerHelper.php
+++ app/Helper/CustomerHelper.php
@@ -87,16 +87,16 @@ class CustomerHelper
 
             // 1.1 If today is after the reward rule valid date
             if ($today->isAfter($current_point_end_date)) {
-                // 1.1.1 First, handle the customer points
                 $this->handleCustomerPoints();
-                
-                // 1.1.2 Set current reward rule configuration to EXPIRED
-                $next_left_over_point_expired_date= Carbon::parse($this->customer->rewardRule->left_over_point_expired_date, Common::SYSTEM_TIMEZONE)->addYear()->toDateTime();
 
-                if ($today->isAfter($next_left_over_point_expired_date)) {
+                if ($today->isAfter($current_left_over_point_expired_date)) {
                     $this->customer->reward_remaining_points = 0;
                 }
-                
+
+
+                // 1.1.2 Set current reward rule configuration to EXPIRED
+                $next_left_over_point_expired_date = Carbon::parse($this->customer->rewardRule->left_over_point_expired_date, Common::SYSTEM_TIMEZONE)->addYear()->toDateTime();
+
                 $this->rewardRuleRepository->update($this->customer->rewardRule->id, ['status' => RewardRuleStatus::EXPIRED]);
 
                 // 1.1.3 Insert new reward rule configuration                                                                                                                                                                                                             
@@ -114,7 +114,7 @@ class CustomerHelper
             }
             // 1.2.1 Handle customer's reward remaining points
             else {
-                if ($this->isRewardRemainingPointInvalid($current_left_over_point_expired_date) && $this->customer->reward_remaining_points) {
+                if ($this->customer->reward_remaining_points) {
                     // Reset remaining points
                     $this->customer->reward_remaining_points = 0;
                 }
@@ -126,7 +126,7 @@ class CustomerHelper
             $activeRewardRule = $this->rewardRuleRepository->findBy([
                 'status' => RewardRuleStatus::ACTIVE
             ]);
-            
+
             // Handle the customer point
             $this->handleCustomerPoints();
 
